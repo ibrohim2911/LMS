d93cc86c8cf21d392b81deb2bd91ff331a370c65
ibrohim2911 <77937292+ibrohim2911@users.noreply.github.com>
2026-04-03 09:07:14 +0500

    users model admin fix

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 from .models import User, ActiveRefreshToken, Notification
 
 admin.site.register(ActiveRefreshToken)
 admin.site.register(Notification)
-admin.site.register(User)  
 
-
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    # Optional: show extra fields
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ("Extra", {"fields": ("role", "phone_number", "ban_expires_at", "max_allowed", "img")}),
+    )
+    add_fieldsets = DjangoUserAdmin.add_fieldsets + (
+        (None, {"fields": ("role", "phone_number")}),
+    )
+    list_display = ("username", "email", "role", "is_staff", "is_superuser", "is_active")
+    search_fields = ("username", "email")
